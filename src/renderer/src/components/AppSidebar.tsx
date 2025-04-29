11f2a55a7e89976a91d7e1f5ce6d20c206a9eb20
Daniel Aguiar <danieljaguiar@msn.com>
2025-04-29 16:54:37 -0500

    refresh directory

--- src/renderer/src/components/AppSidebar.tsx
+++ src/renderer/src/components/AppSidebar.tsx
@@ -9,7 +9,7 @@ import {
   SidebarSeparator
 } from '@/components/ui/sidebar'
 import useTemplateDirectoryStore from '@/stores/templateDirectoryStore'
-import { FolderOpen, Settings } from 'lucide-react'
+import { FolderOpen, RefreshCw, Settings } from 'lucide-react'
 import { ThemeToggle } from './theme-toggle'
 import { Button } from './ui/button'
 import { ScrollArea } from './ui/scroll-area'
@@ -19,6 +19,7 @@ export function AppSidebar() {
   const templateTree = useTemplateDirectoryStore(
     (state) => state.templateDirectory.templateDirectory
   )
+  const basePath = useTemplateDirectoryStore((state) => state.templateDirectory.basePath)
 
   const openFolderHandler = (): void => {
     window.electronAPI.openFolder()
@@ -27,6 +28,15 @@ export function AppSidebar() {
   const openSettingsHandler = (): void => {
     console.log('Opening settings...')
     // Implement settings functionality here
+  }
+
+  const reloadTemplateDirectoryHandler = (): void => {
+    console.log('Reloading template directory...')
+    if (basePath !== '') {
+      window.electronAPI.openFolder(basePath)
+    } else {
+      console.error('Base path is empty. Cannot reload template directory.')
+    }
   }
 
   return (
@@ -38,6 +48,14 @@ export function AppSidebar() {
               <FolderOpen className="h-5 w-5" />
             </Button>
             <ThemeToggle />
+            <Button
+              variant="ghost"
+              size="icon"
+              onClick={reloadTemplateDirectoryHandler}
+              title="Reload"
+            >
+              <RefreshCw className="h-5 w-5" />
+            </Button>
             <Button variant="ghost" size="icon" onClick={openSettingsHandler} title="Settings">
               <Settings className="h-5 w-5" />
             </Button>
