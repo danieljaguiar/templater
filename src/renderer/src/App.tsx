9468499abb1430aedc9a7fd286bc97486c7a7a74
Daniel Aguiar <danieljaguiar@msn.com>
2025-05-06 16:34:24 -0500

    Adjust panes on main

--- src/renderer/src/App.tsx
+++ src/renderer/src/App.tsx
@@ -7,6 +7,7 @@ import TopBar from './components/TopBar'
 import { ResizableHandle, ResizablePanel, ResizablePanelGroup } from './components/ui/resizable'
 import { ScrollArea } from './components/ui/scroll-area'
 import { Toaster } from './components/ui/toaster'
+import { cn } from './lib/utils'
 
 function App(): JSX.Element {
   return (
@@ -17,30 +18,53 @@ function App(): JSX.Element {
         <ResizablePanelGroup
           style={{ height: 'calc(100vh - 3rem)' }}
           direction="horizontal"
-          autoSaveId={'MainPanelGroup'}
+          autoSaveId={'MainPanelGroupMain'}
         >
-          <ResizablePanel minSize={10}>
-            <PaneWrapper>
-              <TemplatePicker />
-            </PaneWrapper>
+          <ResizablePanel minSize={20}>
+            <div className="flex flex-col h-full">
+              <SectionName name="Templates" />
+              <ResizablePanelGroup
+                style={{ height: '100%' }}
+                direction="horizontal"
+                autoSaveId={'MainPanelGroupTemplate'}
+              >
+                <ResizablePanel minSize={10}>
+                  <PaneWrapper name="Template Picker">
+                    <TemplatePicker />
+                  </PaneWrapper>
+                </ResizablePanel>
+                <ResizableHandle withHandle />
+                <ResizablePanel minSize={25}>
+                  <PaneWrapper name="Template Editor">
+                    <TemplatePane />
+                  </PaneWrapper>
+                </ResizablePanel>
+              </ResizablePanelGroup>
+            </div>
           </ResizablePanel>
-          <ResizableHandle withHandle />
-          <ResizablePanel minSize={25}>
-            <PaneWrapper>
-              <TemplatePane />
-            </PaneWrapper>
-          </ResizablePanel>
-          <ResizableHandle withHandle />
-          <ResizablePanel minSize={15}>
-            <PaneWrapper>
-              <DatasetForm />
-            </PaneWrapper>
-          </ResizablePanel>
-          <ResizableHandle withHandle />
-          <ResizablePanel minSize={10}>
-            <PaneWrapper>
-              <DatasetPicker />
-            </PaneWrapper>
+          <ResizableHandle className="bg-primary/10 border-l border-r w-7" withHandle />
+
+          <ResizablePanel minSize={20}>
+            <div className="flex flex-col h-full">
+              <SectionName align="right" name="Datasets" />
+              <ResizablePanelGroup
+                style={{ height: '100%' }}
+                direction="horizontal"
+                autoSaveId={'MainPanelGroupDataset'}
+              >
+                <ResizablePanel minSize={15}>
+                  <PaneWrapper name="Dataset Editor">
+                    <DatasetForm />
+                  </PaneWrapper>
+                </ResizablePanel>
+                <ResizableHandle withHandle />
+                <ResizablePanel minSize={10}>
+                  <PaneWrapper name="Dataset Picker">
+                    <DatasetPicker />
+                  </PaneWrapper>
+                </ResizablePanel>
+              </ResizablePanelGroup>
+            </div>
           </ResizablePanel>
         </ResizablePanelGroup>
         <Toaster />
@@ -49,10 +73,31 @@ function App(): JSX.Element {
   )
 }
 
-function PaneWrapper(props: { children: React.ReactNode }) {
+function PaneWrapper(props: { children: React.ReactNode; name: string }) {
   return (
     <div className="pl-4 pr-1 py-2 h-full">
-      <ScrollArea className="h-full pr-4">{props.children}</ScrollArea>
+      <ScrollArea className="h-full pr-4">
+        <div className="w-full text-2xl font-bold text-muted-foreground bg-background border-b px-4 py-3 mb-2">
+          {props.name}
+        </div>
+
+        {props.children}
+      </ScrollArea>
+    </div>
+  )
+}
+
+function SectionName(props: { name: string; align?: 'left' | 'right' }) {
+  return (
+    <div
+      className={cn(
+        'flex items-center px-4 py-3 text-sm font-medium text-muted-foreground bg-background border-b ',
+        'text-4xl font-extrabold',
+        // props.align === 'right' ? 'justify-end' : 'justify-start'
+        'justify-center'
+      )}
+    >
+      {props.name}
     </div>
   )
 }
